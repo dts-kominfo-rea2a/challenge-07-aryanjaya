f42c54f8cb3cc5cad777e17fe2c8c89114749bea
Aryan Jaya <aryanjaya88@gmail.com>
2022-06-10 08:45:34 +0800

    finish test case

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  const data = [
+    ...await promiseTheaterIXX(),
+    ...await promiseTheaterVGC(),
+  ];
+
+  return data.reduce(
+    (total, film) => film.hasil === emotion ? ++total: total,
+    0
+  );
+};
 
 module.exports = {
   promiseOutput,
